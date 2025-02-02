00f9e520a8dc6ea28a104fe8807ab62cfbe06197
MrUse77 <agusdor14@hotmail.com>
2025-02-02 19:09:01 -0300

    Mejora la configuración de CORS para permitir métodos y encabezados específicos

--- app.js
+++ app.js
@@ -10,7 +10,13 @@ const experienciaRouter = require("./routes/experiencia");
 const storageRouter = require("./routes/storage");
 require("dotenv").config();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: "GET,POST,PUT,DELETE",
+    allowedHeaders: "Content-Type, Authorization",
+  })
+);
 app.use(express.json());
 app.use(express.static("storage"));
 
