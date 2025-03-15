d5608e6db889e7c95bb0a9271e05bc86d2f47ee8
MrUse77 <agusdor14@hotmail.com>
2025-03-15 17:51:07 -0300

    Actualizar archivo .env y eliminar la ruta de almacenamiento

--- routes/storage.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const storageRouter = express.Router();
-const upload = require("../utils/handleStorage");
-const { createFile, generatePublicUrl } = require("../controllers/storage");
-
-storageRouter.post("/create", upload.single("myfile"), createFile);
-storageRouter.get("/get", generatePublicUrl);
-
-module.exports = storageRouter;
